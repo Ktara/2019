96a7045ad4ee276ed3b533dc5cec3662efb562bb
ktara <ktaraash@gmail.com>
2019-01-14 14:35:05 +0200

    instuctor page other

--- issa/assets/js/script.js
+++ issa/assets/js/script.js
@@ -1,3 +1,28 @@
 $('.cardContainer').mouseenter(function(){
     $(this).find('.card').toggleClass('flipped');
 });
+
+
+
+
+
+
+$('.instructor_gallery_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    infinite: true,
+});
+$('.instructor_gallery_slider_nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.instructor_gallery_slider',
+    dots: false,
+    centerMode: false,
+    infinite: true,
+    focusOnSelect: true,
+    arrows: true,
+    prevArrow: '<div class="instructor_gallery_prewAr"></div>',
+    nextArrow: '<div class="instructor_gallery_nextAr"></div>'
+});
