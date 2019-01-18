287385dc51c7924bd8d3a11f30031632d67f9d6c
ktara <ktaraash@gmail.com>
2019-01-18 14:09:13 +0200

    video

--- issa/assets/js/script.js
+++ issa/assets/js/script.js
@@ -26,3 +26,17 @@ $('.instructor_gallery_slider_nav').slick({
     prevArrow: '<div class="instructor_gallery_prewAr"></div>',
     nextArrow: '<div class="instructor_gallery_nextAr"></div>'
 });
+
+
+
+
+
+
+$('.main_video_play').click(function(){
+    $('.main_video_wrapper').addClass('open_wrapper').velocity({
+        opacity: 1
+    }, 600);
+    $('.main_video_play').css({
+        visibility: 'hidden'
+    })
+});
