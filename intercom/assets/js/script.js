9dc1e61738affc86ec82852dad9cbaa7a8c59b05
Ktara <ktaraah@gmail.com>
2019-07-10 13:34:53 +0300

    main3

--- intercom/assets/js/script.js
+++ intercom/assets/js/script.js
@@ -2,3 +2,21 @@ $('.nav_header_burger').click(function(){
     $('.popup_nav_menu').toggleClass('popup_nav_menu_active');
     $('.nav_header_burger').toggleClass('nav_header_burger_close');
 });
+
+
+$('.text_block_list_point_title').click(function(){
+    $('.text_block_list_point_wrapper').animate({
+        height: 0
+    }, 400);
+    var target = $(this).next();
+    if ($(this).hasClass('text_block_list_point_title_open')){
+        $(this).removeClass('text_block_list_point_title_open');
+        return;
+    };
+    $('.main_cooperate_list_point_title').removeClass('text_block_list_point_title_open');
+    var childHeight = target.children().height();
+    target.animate({
+        height: childHeight
+    }, 400);
+    $(this).addClass('text_block_list_point_title_open');
+});
