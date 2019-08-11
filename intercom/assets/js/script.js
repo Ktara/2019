8a01c33e29f4f83db607e9ea62a3c715e947cd1c
Ktara <ktaraah@gmail.com>
2019-08-11 20:39:32 +0300

    gallery slider

--- intercom/assets/js/script.js
+++ intercom/assets/js/script.js
@@ -131,9 +131,9 @@ $('.main_slider').slick({
 
 $('.review_slider').slick({
     centerMode: true,
-    centerPadding: '20px',
+    centerPadding: '27%',
     // focusOnSelect: true,
-    slidesToShow: 2,
+    slidesToShow: 1,
     slidesToScroll: 1,
     nextArrow: '<div class="review_next_arrow"><img src="assets/img/icon/arrow-right.svg" alt=""></div>',
     prevArrow: '<div class="review_prev_arrow"><img src="assets/img/icon/arrow-right.svg" alt=""></div>',
@@ -159,137 +159,48 @@ $('.review_slider').slick({
 
 
 
-// $(document).ready(function(){
-//     var menuItemNum=$(".main_media_item").length;
-//     var angle=120;
-//     var distance=90;
-//     var startingAngle=180+(-angle/2);
-//     var slice=angle/(menuItemNum-1);
-//     TweenMax.globalTimeScale(0.8);
-//     $(".main_media_item").each(function(i){
-//         var angle=startingAngle+(slice*i);
-//         $(this).css({
-//             transform:"rotate("+(angle)+"deg)"
-//         })
-//         $(this).find(".menu-item-icon").css({
-//             transform:"rotate("+(-angle)+"deg)"
-//         })
-//     })
-//     var on=false;
-//
-//     $(".menu-toggle-button").mousedown(function(){
-//         TweenMax.to($(".menu-toggle-icon"),0.1,{
-//             scale:0.65
-//         })
-//     })
-//     $(document).mouseup(function(){
-//         TweenMax.to($(".menu-toggle-icon"),0.1,{
-//             scale:1
-//         })
-//     });
-//     $(document).on("touchend",function(){
-//         $(document).trigger("mouseup")
-//     })
-//     $(".menu-toggle-button").on("mousedown",pressHandler);
-//     $(".menu-toggle-button").on("touchstart",function(event){
-//         $(this).trigger("mousedown");
-//         event.preventDefault();
-//         event.stopPropagation();
-//     });
-//
-//     function pressHandler(event){
-//         on=!on;
-//
-//         TweenMax.to($(this).children('.menu-toggle-icon'),0.4,{
-//             rotation:on?45:0,
-//             ease:Quint.easeInOut,
-//             force3D:true
-//         });
-//
-//         on?openMenu():closeMenu();
-//
-//     }
-//     function openMenu(){
-//         $(".main_media_item").each(function(i){
-//             var delay=i*0.08;
-//
-//             var $bounce=$(this).children(".menu-item-bounce");
-//
-//             TweenMax.fromTo($bounce,0.2,{
-//                 transformOrigin:"50% 50%"
-//             },{
-//                 delay:delay,
-//                 scaleX:0.8,
-//                 scaleY:1.2,
-//                 force3D:true,
-//                 ease:Quad.easeInOut,
-//                 onComplete:function(){
-//                     TweenMax.to($bounce,0.15,{
-//                         // scaleX:1.2,
-//                         scaleY:0.7,
-//                         force3D:true,
-//                         ease:Quad.easeInOut,
-//                         onComplete:function(){
-//                             TweenMax.to($bounce,3,{
-//                                 // scaleX:1,
-//                                 scaleY:0.8,
-//                                 force3D:true,
-//                                 ease:Elastic.easeOut,
-//                                 easeParams:[1.1,0.12]
-//                             })
-//                         }
-//                     })
-//                 }
-//             });
-//
-//             TweenMax.to($(this).children(".menu-item-button"),0.5,{
-//                 delay:delay,
-//                 y:distance,
-//                 force3D:true,
-//                 ease:Quint.easeInOut
-//             });
-//         })
-//     }
-//     function closeMenu(){
-//         $(".main_media_item").each(function(i){
-//             var delay=i*0.08;
-//
-//             var $bounce=$(this).children(".menu-item-bounce");
-//
-//             TweenMax.fromTo($bounce,0.2,{
-//                 transformOrigin:"50% 50%"
-//             },{
-//                 delay:delay,
-//                 scaleX:1,
-//                 scaleY:0.8,
-//                 force3D:true,
-//                 ease:Quad.easeInOut,
-//                 onComplete:function(){
-//                     TweenMax.to($bounce,0.15,{
-//                         // scaleX:1.2,
-//                         scaleY:1.2,
-//                         force3D:true,
-//                         ease:Quad.easeInOut,
-//                         onComplete:function(){
-//                             TweenMax.to($bounce,3,{
-//                                 // scaleX:1,
-//                                 scaleY:1,
-//                                 force3D:true,
-//                                 ease:Elastic.easeOut,
-//                                 easeParams:[1.1,0.12]
-//                             })
-//                         }
-//                     })
-//                 }
-//             });
-//
-//
-//             TweenMax.to($(this).children(".menu-item-button"),0.3,{
-//                 delay:delay,
-//                 y:0,
-//                 force3D:true,
-//                 ease:Quint.easeIn
-//             });
-//         })
-//     }
-// })
+
+$('.gallery_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    nextArrow: '<div class="gallery_next_arrow">Дальше</div>',
+    prevArrow: '<div class="gallery_prev_arrow">Назад</div>',
+    fade: true,
+    infinite: true,
+    customPaging : function(slider, i) {
+        var length = slider.$slides.length;
+        $('.gallery_count').text(length);
+        var idx = i + 1;
+        return '0' + idx;
+    }
+});
+$('.gallery_slider_nav').slick({
+    centerMode: true,
+    centerPadding: '48.5%',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.gallery_slider',
+    dots: false,
+    arrows: false,
+    infinite: true,
+    focusOnSelect: true,
+    vertical: true,
+    verticalSwiping: true,
+    responsive: [
+        {
+            breakpoint: 1440,
+            settings: {
+                centerPadding: '66.5%',
+            }
+        },
+        {
+            breakpoint: 420,
+            settings: {
+                slidesToShow: 1,
+                centerPadding: '20px'
+            }
+        }
+    ]
+});
