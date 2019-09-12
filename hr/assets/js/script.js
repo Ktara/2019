769f0ffae078d336cd90f76c36fb5eee5b28b15e
Ktara <ktaraah@gmail.com>
2019-09-12 14:05:58 +0300

    events page

--- hr/assets/js/script.js
+++ hr/assets/js/script.js
@@ -6,3 +6,22 @@ $('.main_slider').slick({
     dots: true,
     arrows: true
 });
+
+
+
+$('.blog_categories_point').click(function(){
+    $('.blog_categories_wrapper').animate({
+        height: 0
+    }, 400);
+    var target = $(this).next();
+    if ($(this).hasClass('blog_categories_active')){
+        $(this).removeClass('blog_categories_active');
+        return;
+    };
+    $('.blog_categories_point').removeClass('blog_categories_active');
+    var childHeight = target.children().height();
+    target.animate({
+        height: childHeight
+    }, 400);
+    $(this).addClass('blog_categories_active');
+});
